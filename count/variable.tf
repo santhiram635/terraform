353fe39a4d529f227c455d41acacf4ed5f31c6df
prasanthi <santhi@ram.com>
2025-10-15 20:49:34 +0530

    terraform

--- count/variable.tf
+++ count/variable.tf
@@ -1,3 +1,14 @@
 variable "instances" {
   default = [ "mongodb","redis","mysql","rabbitmq" ]
 }
+
+variable "zone_id" {
+    default = "Z0276032366IKHANOML69"
+  
+}
+
+variable "domain_name" {
+    default = "mokshi.fun"
+  
+}
+
