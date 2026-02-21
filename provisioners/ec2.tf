c99eb25ba1ba688dae4dbb8d123e7fd6c2800e88
prasanthi <santhi@ram.com>
2026-02-21 16:13:14 +0530

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -1,49 +1,43 @@
 resource "aws_instance" "terraform" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    ami = "ami-00ca32bbc84273381"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = "terraform"
+        Terraform = "true"
+    }
 
-  tags = {
-    Name = "terraform"
-    terraform= "true"
-  }
+    provisioner "local-exec"{
+      command = "echo ${self.private_ip} > inventory"
+      on_failure = continue
+    }
 
-  provisioner "local-exec" {
-    command = "echo ${self.private_ip} > inventory"
-    on_failure = continue
-    
-  }
+    provisioner "local-exec"{
+      command = "echo Instance is destroyed"
+      when    = destroy
+    }
 
-  provisioner "local-exec" {
-    command = "echo instance is destory"
-    when = destroy
-    
-  }
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = self.public_ip
-  }
-
-  provisioner "remote-exec" {
+    provisioner "remote-exec" {
       inline = [
         "sudo dnf install nginx -y",
         "sudo systemctl start nginx"
       ]
-  }
+    }
 
-  provisioner "remote-exec" {
-    inline = [ 
-      "sudo systemctl stop nginx",
-      "echo 'successfully stoped nginx server' "
-    ]
-    when = destroy
-    
-  }
-
-
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stopped nginx server' "
+      ]
+      when = destroy
+    }
 }
 
 resource "aws_security_group" "allow_all" {
