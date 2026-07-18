0b0add08cece4c9cb1412eefd903c83b57494630
chereddy prasanthi <santhiram635@gmail.com>
2026-07-18 15:30:22 +0530

    remote-state

--- remote-state/ec2.tf
+++ remote-state/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-  ami           = "ami-00ca32bbc84273381"
+  ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
 
