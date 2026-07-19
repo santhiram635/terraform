f72e0b6760b3e7693592d9410bb841a63ea84b05
chereddy prasanthi <santhiram635@gmail.com>
2026-07-19 12:22:20 +0530

    terraform

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   common_name = "${var.project}-${var.environment}" #roboshop-dev
   ami_id = data.aws_ami.joindevops.id
   ec2_tags = merge(
