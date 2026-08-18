fd07d6c9647409ee00a4a00614ef06021bc4ac84
chereddy prasanthi <santhiram635@gmail.com>
2026-08-18 15:23:34 +0530

    terraform

---
+++ import/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "demo" {
+  instance_type = "t3.micro"
+  ami           = "ami-0220d79f3f480ecf5"
+  vpc_security_group_ids = [
+    "sg-04eea70f920ee6e11"
+  ]
+  tags = {
+    Name = "import-demo-change"
+  }
+
+}
